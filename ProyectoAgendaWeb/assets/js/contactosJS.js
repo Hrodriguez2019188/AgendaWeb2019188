8fec52c1ce64f914caa10bf79a31b3487d37b797
Hrodriguez2019188 <hrodriguez-2019188@kinal.edu.gt>
2024-01-31 23:29:43 -0600

    se creo la pagiana favoritos, que es una seleccion de algunas partes de contactos y se implemento con el mismo script y css

--- ProyectoAgendaWeb/assets/js/contactosJS.js
+++ ProyectoAgendaWeb/assets/js/contactosJS.js
@@ -18,6 +18,25 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById('ventana-info-usuario-modal');
+    var btn = document.getElementById('info-usuario-modal');
+    var span = document.getElementsByClassName('close')[0];
+
+    btn.addEventListener('click', function() {
+        modal.style.display = 'block';
+    });
+
+    span.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     var modal = document.getElementById('ventana-info-usuario-modal');
